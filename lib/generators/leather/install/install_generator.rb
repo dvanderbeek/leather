e28f14b54875abc5c787fae00e8f5d723c276a64
David Van Der Beek <earlynovrock@gmail.com>
2014-07-14 13:35:02 -0500

    move devise views and config file into templates folder

--- lib/generators/leather/install/install_generator.rb
+++ lib/generators/leather/install/install_generator.rb
@@ -1,6 +1,6 @@
 module Leather
   class InstallGenerator < Rails::Generators::Base
-    source_root File.expand_path('../../../../../app', __FILE__)
+    source_root File.expand_path('../../../../../templates', __FILE__)
 
     def copy_views
       directory("views/devise", Rails.root.join("app", "views", "devise"))
@@ -11,11 +11,11 @@ module Leather
     end
 
     def copy_ui_kit_styles
-      copy_file("assets/stylesheets/bootstrap_variables.css.scss", Rails.root.join("app", "assets", "stylesheets", "bootstrap_variables.css.scss"))
+      copy_file("bootstrap_variables.css.scss", Rails.root.join("app", "assets", "stylesheets", "bootstrap_variables.css.scss"))
     end
 
     def copy_high_voltage_config
-      copy_file("config/initializers/high_voltage.rb", Rails.root.join("config", "initializers", "high_voltage.rb"))
+      copy_file("high_voltage.rb", Rails.root.join("config", "initializers", "high_voltage.rb"))
     end
   end
 end
