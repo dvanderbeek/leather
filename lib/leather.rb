795d351c9f3eb590a1bcee6a1e4c661f2f152065
David Van Der Beek <earlynovrock@gmail.com>
2014-08-14 12:28:11 -0500

    require dependency gems

--- lib/leather.rb
+++ lib/leather.rb
@@ -1,3 +1,8 @@
+require 'devise'
+require 'bootstrap-sass'
+require 'high_voltage'
+require 'haml-rails'
+
 require 'leather/engine'
 require 'leather/action_controller_extension'
 require 'leather/railtie'
